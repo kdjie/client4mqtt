cb6c0bff14025854f5a022365b09ec2ea0ddad22
kdjie <14166097@qq.com>
2021-08-17 10:18:13 +0800

    线程安全保护

--- ThreadModel.cpp
+++ ThreadModel.cpp
@@ -2,9 +2,13 @@
 
 #ifdef __linux__
 #include <unistd.h>
+#include <sys/time.h>
 #endif
 
-#include <time.h>
+#ifdef __windows__
+#include <windows.h>
+#include <sys/timeb.h>
+#endif
 
 dakuang::CEventQueue::CEventQueue()
 {
@@ -30,8 +34,26 @@ void dakuang::CEventQueue::pushEvent(const std::string& strEvent, void* pData)
 bool dakuang::CEventQueue::popEvent(std::string& strEvent, void** _pData, int nMinSeconds)
 {
     struct timespec absTime;
-    absTime.tv_sec = time(NULL) + nMinSeconds / 1000;
-    absTime.tv_nsec = (nMinSeconds % 1000) * 1000000;
+
+#ifdef __linux__
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    absTime.tv_sec = tv.tv_sec + nMinSeconds / 1000;
+    absTime.tv_nsec = tv.tv_usec * 1000 + (nMinSeconds % 1000) * 1000000;
+#endif
+
+#ifdef __windows__
+    timeb tb;
+    ftime(&tb);
+    absTime.tv_sec = tb.time + nMinSeconds / 1000;
+    absTime.tv_nsec = tb.millitm * 1000000 + (nMinSeconds % 1000) * 1000000;
+#endif
+
+    if (absTime.tv_nsec >= 1000000000)
+    {
+        absTime.tv_sec += 1;
+        absTime.tv_nsec -= 1000000000;
+    }
     sem_timedwait(&m_sem, &absTime);
 
     pthread_mutex_lock(&m_mutex);
@@ -53,23 +75,34 @@ bool dakuang::CEventQueue::popEvent(std::string& strEvent, void** _pData, int nM
 
 dakuang::CThreadModel::CThreadModel()
 {
-    m_bRuning = false;
+    pthread_mutex_init(&m_mutexEventCB, NULL);
+
+    m_bRunning = false;
 }
 
 dakuang::CThreadModel::~CThreadModel()
 {
+    // 确保线程安全退出
+    this->quit();
+    this->wait();
+
+    pthread_mutex_destroy(&m_mutexEventCB);
 }
 
 // 注册事件回调
 void dakuang::CThreadModel::registerEventCB(const std::string& strEvent, dakuang::CThreadModel::CALLBACK_FUNC_t func)
 {
+    pthread_mutex_lock(&m_mutexEventCB);
     m_mapEventCB.insert( std::make_pair(strEvent, func) );
+    pthread_mutex_unlock(&m_mutexEventCB);
 }
 
 // 取消注册事件回调
 void dakuang::CThreadModel::unRegisterEventCB(const std::string& strEvent)
 {
+    pthread_mutex_lock(&m_mutexEventCB);
     m_mapEventCB.erase(strEvent);
+    pthread_mutex_unlock(&m_mutexEventCB);
 }
 
 // 发射指定事件
@@ -81,20 +114,20 @@ void dakuang::CThreadModel::emitEvent(const std::string& strEvent, void* pData)
 // 启动线程
 bool dakuang::CThreadModel::start()
 {
-    if (m_bRuning)
+    if (m_bRunning)
         return true;
 
-    if (pthread_create(&m_tidWorking, NULL, __threadbody, this) != 0)
+    if (pthread_create(&m_tidWorking, NULL, __threadBody, this) != 0)
         return false;
 
-    m_bRuning = true;
+    m_bRunning = true;
     return true;
 }
 
 // 退出线程
 void dakuang::CThreadModel::quit()
 {
-    if (!m_bRuning)
+    if (!m_bRunning)
         return;
 
     m_eventQueue.pushEvent("quit");
@@ -102,25 +135,25 @@ void dakuang::CThreadModel::quit()
 
 void dakuang::CThreadModel::wait()
 {
-    if (!m_bRuning)
+    if (!m_bRunning)
         return;
 
     pthread_join(m_tidWorking, NULL);
 
-    m_bRuning = false;
+    m_bRunning = false;
 }
 
 // 静态线程函数
-void *dakuang::CThreadModel::__threadbody(void *_lp)
+void *dakuang::CThreadModel::__threadBody(void *_lp)
 {
     dakuang::CThreadModel* pThis = (dakuang::CThreadModel*)_lp;
 
-    pThis->__threadloop();
+    pThis->__threadLoop();
     return NULL;
 }
 
 // 工作线程主体
-void dakuang::CThreadModel::__threadloop()
+void dakuang::CThreadModel::__threadLoop()
 {
     __routerEvent("start", NULL);
 
@@ -143,17 +176,21 @@ void dakuang::CThreadModel::__threadloop()
 // 路由事件
 void dakuang::CThreadModel::__routerEvent(const std::string& strEvent, void* pData)
 {
+    pthread_mutex_lock(&m_mutexEventCB);
+
     MAP_EVENT_CB_t::iterator iter = m_mapEventCB.find(strEvent);
     if (iter != m_mapEventCB.end())
     {
         iter->second(pData);
     }
+
+    pthread_mutex_unlock(&m_mutexEventCB);
 }
 
 // 设置定时回调
 dakuang::CThreadTimer::CThreadTimer()
 {
-    m_nMinSeconds = 10000;
+    m_nMinSeconds = 1000;
     m_bRunning = false;
 }
 
@@ -165,32 +202,39 @@ void dakuang::CThreadTimer::setTimerCB(dakuang::CThreadTimer::CALLBACK_FUNC_t cb
 // 启动/停止定时器
 bool dakuang::CThreadTimer::start(int nMinSeconds)
 {
-    m_nMinSeconds = nMinSeconds;
-    m_bRunning = true;
+    if (m_bRunning)
+        return true;
 
-    if (pthread_create(&m_tidWorking, NULL, __threadbody, this) != 0)
+    m_nMinSeconds = nMinSeconds;
+
+    if (pthread_create(&m_tidWorking, NULL, __threadBody, this) != 0)
         return false;
 
+    m_bRunning = true;
     return true;
 }
 
 void dakuang::CThreadTimer::stop()
 {
-    m_bRunning = false;
+    if (!m_bRunning)
+        return;
+
     pthread_join(m_tidWorking, NULL);
+
+    m_bRunning = false;
 }
 
 // 静态线程函数
-void *dakuang::CThreadTimer::__threadbody(void *_lp)
+void *dakuang::CThreadTimer::__threadBody(void *_lp)
 {
     dakuang::CThreadTimer* pThis = (dakuang::CThreadTimer*)_lp;
 
-    pThis->__threadloop();
+    pThis->__threadLoop();
     return NULL;
 }
 
 // 工作线程主体
-void dakuang::CThreadTimer::__threadloop()
+void dakuang::CThreadTimer::__threadLoop()
 {
     while (m_bRunning)
     {
@@ -213,9 +257,11 @@ void dakuang::CThreadTimer::__delayInterval()
     for (int i = 0; i < nTimes && m_bRunning; ++i)
     {
 #ifdef __linux__
-        usleep(100000);
-#else
-        Sleep(100);
+        ::usleep(100000);
+#endif
+
+#ifdef __windows__
+        ::Sleep(100);
 #endif
     }
 }
